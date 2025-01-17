fcb20f01ae2d1c46e4efc55f0c81ee8c32e286bd
AmbiShanthi19 <ambishanthi19@gmail.com>
2025-01-17 10:54:30 +0530

    Program3 of Task6

---
+++ CircleMain.java
@@ -0,0 +1,14 @@
+
+
+package task6;
+
+public class CircleMain {
+
+	public static void main(String[] args) {
+		Circle cwo=new Circle();
+		System.out.println(" The circumference of the circle is " +cwo.getCircumference());
+		Circle cwi=new Circle(4.56);
+		System.out.println("The circumference of the circle is " +cwi.getCircumference());
+	}
+
+}
